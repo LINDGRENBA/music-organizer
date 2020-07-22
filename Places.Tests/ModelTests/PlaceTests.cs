334f79eb32348a0b52cc6f1e5495a983d3907254
Kate Skorija <kateskorija@gmail.com>
2020-07-22 10:51:11 -0700

    set up file tree
    Co-authored-by: Brittany Lindgren <brittdevelopsit@gmail.com>

---
+++ Places.Tests/ModelTests/PlaceTests.cs
@@ -0,0 +1,11 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Places.Models;
+
+namespace Places.Tests
+{
+  [TestClass]
+  public class PlaceTests
+  {
+    
+  }
+}
---
+++ Places/Controllers/PlacesController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+using Places.Models;
+
+namespace Places.Controllers
+{
+  public class PlacesController : Controller
+  {
+    //Routes go here
+  }
+}
